f8c73eb4ee702b7bc136ac735a1939c348363301
TuretskayaIrina <turetskaya30.07.1994@gmail.com>
2020-10-03 20:26:09 +0300

    feat: auth

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -2,17 +2,17 @@ import React from 'react';
 import Error from '../images/error.png';
 import Ok from '../images/ok.png';
 
-function InfoTooltip() {
+function InfoTooltip(props) {
   return (
-    <div className="popup popup_opened popup_bg-opacity-light">
+    <div className={(props.isOpen ? `popup popup_opened popup_bg-opacity-light` : `popup popup_bg-opacity-light`)}>
       <div className="popup__container">
         <div className="infotooltip">
           <div className="infotooltip__container">
-            <img alt="status" src={Ok}></img>
-            <h3 className="infotooltip__title">Вы успешно зарегистрировались!</h3>
+            <img alt="status" src={`${ props.loggedIn ? Ok : Error }`}></img>
+            <h3 className="infotooltip__title">{`${ props.loggedIn ? 'Вы успешно зарегистрировались!' : 'Что-то пошло не так!Попробуйте ещё раз.'}`}</h3>
           </div>
         </div>
-        <button className="popup__button-close" type="button"></button>
+        <button className="popup__button-close" type="button" onClick={props.onClose}></button>
       </div>
     </div>
   );
---
+++ src/utils/auth.js
@@ -0,0 +1,66 @@
+import {authUrl} from './utils';
+
+// запросить регистрацию пользователя
+export const register = (email, password) => {
+  return fetch(
+    `${authUrl}/signup`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, password})
+    }
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      else {
+        return Promise.reject(`Error: ${res.status}`);
+      }
+    })
+}
+
+// запросить авторизацию пользователя
+export const authorize = (email, password) => {
+  return fetch(
+    `${authUrl}/signup`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email, password})
+    }
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      else {
+        return Promise.reject(`Error: ${res.status}`);
+      }
+    })
+    .catch((err) => console.log(err))
+}
+
+// проверить валидность токена и получить email для вставки в шапку сайта
+export const getContent = (token) => {
+  return fetch(`${authUrl}/users/me`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`,
+    }
+  })
+    .then((res) => {
+      if (res.ok) {
+          return res.json();
+      }
+      else {
+          return Promise.reject(`Error: ${res.status}`);
+      }
+    })
+    .catch(err => console.log(err))
+}
