aa0669ad5b11ff013516c7e4b6ab78b1976c2f3d
TuretskayaIrina <turetskaya30.07.1994@gmail.com>
2020-10-22 21:46:33 +0300

    feat: api

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,13 @@
 // мой конфиг
 
 const config = {
-  baseUrl: 'https://api.kusto.students.nomoreparties.xyz',
+  baseUrl: 'https://api.cat.students.nomoreparties.space',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
   }
 };
 
-export const authUrl = 'https://api.kusto.students.nomoreparties.xyz';
+export const authUrl = 'https://api.cat.students.nomoreparties.space';
 
 export default config;
